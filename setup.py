9dd1c4854bfc8b4e98e6488ada5183ba54273ccc
Nirmal Kumar <rkumarnirmal@gmail.com>
2012-08-04 13:14:34 +0530

    Project Version -0.1.1-

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='vimeo',
-    version='0.1.0',
+    version='0.1.1',
     author='Nirmal Kumar',
     author_email='rkumarnirmal@gmail.com',
     packages=['vimeo'],
     scripts=['vimeo/client.py'],
     url='https://github.com/rkumarnirmal/python-vimeo',
     license='MIT',
-    packages=['vimeo'],
     description='Python module for using Vimeo API.',
     install_requires=[
         "requests == 0.13.1",
