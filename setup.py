b2e84be7d59a1e76cad253510db57f07ed69c583
Nirmal Kumar <rkumarnirmal@gmail.com>
2012-08-04 12:28:29 +0530

    Added README and setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+m distutils.core import setup
+
+setup(
+    name='TowelStuff',
+    version='0.1.0',
+    author='J. Random Hacker',
+    author_email='jrh@example.com',
+    packages=['towelstuff', 'towelstuff.test'],
+    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests == 0.13.1",
+        "caldav == 0.1.4",
+    ],
+)
