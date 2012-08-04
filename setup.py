726c9d0ac52806418025f88026c030f51fae0628
Nirmal Kumar <rkumarnirmal@gmail.com>
2012-08-04 16:11:02 +0530

    Newly modified script

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
-from distutils.core import setup
+import re
+from setuptools import setup
+
+version = None
+for line in open('./vimeo/__init__.py'):
+    m = re.search('__version__\s*=\s*(.*)', line)
+    if m:
+        version = m.group(1).strip()[1:-1]  # quotes
+        break
+assert version
 
 setup(
     name='vimeo',
-    version='0.1.1',
+    version=version,
+    description='Python module for using Vimeo API.',
     author='Nirmal Kumar',
     author_email='rkumarnirmal@gmail.com',
-    packages=['vimeo'],
-    scripts=['vimeo/client.py'],
     url='https://github.com/rkumarnirmal/python-vimeo',
+    packages=['vimeo'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='vimeo internet api',
+    scripts=['vimeo/client.py'],
     license='MIT',
-    description='Python module for using Vimeo API.',
     install_requires=[
         "requests == 0.13.1",
         "simplejson >= 2.1.6",
